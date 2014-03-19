9fae326ebdd4ead19aa403ae95024d0331c1f2de
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2014-03-19 11:17:49 -0600

    change setup

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ if [ ! -d venv ]; then
     virtualenv venv
 fi
 
-[[ "$VIRTUAL_ENV" == "" ]];. venv/bin/activate
-pip install -U pil MySQL-python django django-treebeard django-cors-headers djangorestframework django-filter celery defusedxml django-tastypie jsonfield  django-activity-stream lxml feedparser --allow-external PIL --allow-unverified PIL 
-easy_install South
+[[ "$VIRTUAL_ENV" == ""  ]];. venv/bin/activate
+pip install -U pil MySQL-python django django-treebeard django-cors-headers djangorestframework django-filter celery defusedxml django-tastypie jsonfield django-activity-stream lxml feedparser --allow-external PIL --allow-unverified PIL 
 
-celery worker --app=api -l info
+#celery worker --app=api -l info
 
 # useradd -M celery
 # usermod -L celery
