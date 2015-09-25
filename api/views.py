c66fc16e4463be556d16517b9a0bfcf4d8ad6f5d
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2015-09-25 14:25:03 -0600

    change image cache

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_control
+from django.utils.cache import patch_cache_control, add_never_cache_headers
 from django.db.models import Manager
 
 from rest_framework import status, filters
@@ -187,13 +188,16 @@ class APIView(CreateModelMixin, RelationView):
                 action=action, data={'doc': doc.get_urn()})
         return self.create(data=data)
 
-    @method_decorator(cache_control(private=True, max_age=3600))
     def _get_has_image(self, request, *args, **kwargs):
         zoom = self.kwargs.get('zoom', None)
         x = self.kwargs.get('x', None)
         y = self.kwargs.get('y', None)
-        return self.get_response(
-            self.get_object().has_image(zoom=zoom, x=x, y=y))
+        response = self.get_object().has_image(zoom=zoom, x=x, y=y)
+        if response:
+            response = self.get_response(response)
+            patch_cache_control(response, private=True, max_age=3600)
+            return response
+        return self.get_response(response)
 
     def _get_xmlvalidate(self, request, *args, **kwargs):
         xml = request.REQUEST.get('xml', None)
